8918ee4f730c3e098026563a9fe4e3a894bca87a
Andrea Bachechi <Bak@iMac.lan>
2020-03-28 17:17:40 +0100

    tetsing

--- message_reader.py
+++ message_reader.py
@@ -1,4 +1,5 @@
 def message_handler(update, context):
     user_says = " ".join(context.args)
-    update.message.reply_text("You said: " + user_says)
+    update.message.reply_text("Il tuo messaggio è stato salvato")
+    #update.message.reply_text("You said: " + user_says)
 
